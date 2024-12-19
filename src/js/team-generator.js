2cfdb96cc1424a5add681a76dfa9e49401d1c16f
yulti879 <yulti879@gmail.com>
2024-12-19 15:35:05 +0300

    генератор реализован

---
+++ src/js/team-generator.js
@@ -0,0 +1,15 @@
+export default class Team {
+    constructor() {
+        this.members = [];
+    }
+    
+    add(character) {
+        this.members.push(character);
+    }
+    
+    *[Symbol.iterator]() {
+        for (const member of this.members)  {
+            yield member;
+        }
+    }
+}
